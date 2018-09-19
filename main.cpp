275f04eba0f8a5283011c29b653ccc181d3c73b5
Jeremy D Rhodes <jdrhodes@ecc-jaguar.csuchico.edu>
2018-09-19 16:12:38 -0700

    Asks for first and last name

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors:Jerm Deeezy (Rhodes)
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,13 @@ using namespace std;
 int main()
 {
   //declare variables
-
+string first;
+string last;
   //get user input
-
+cout<<"Please enter your first name: ";
+cin>>first;
+cout<<"Please enter your last name: ";
+cin>>last;
   //tell fortune
 
   return 0;
