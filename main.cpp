816a16eb5c61e30e8fa43dd8a652e16255d45238
Jeremy D Rhodes <jdrhodes@ecc-jaguar.csuchico.edu>
2018-09-19 16:22:53 -0700

    Determines User's Fortune (Lucky Number)

--- main.cpp
+++ main.cpp
@@ -9,12 +9,16 @@ int main()
   //declare variables
 string first;
 string last;
+int lucky;
   //get user input
 cout<<"Please enter your first name: ";
 cin>>first;
 cout<<"Please enter your last name: ";
 cin>>last;
   //tell fortune
-
+  lucky = first.length();
+cout<<"Welcome, "<< first[0] << last[0] << " here is your fortune...\n";
+cout<<"Your lucky number is..."<<lucky<<"\n";
   return 0;
+  
 }
