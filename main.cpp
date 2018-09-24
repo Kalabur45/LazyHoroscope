05daf2974612eb3f3022f4b3c725614cf9bda855
Jeremy D Rhodes <jdrhodes@ecc-jaguar.csuchico.edu>
2018-09-24 16:44:48 -0700

    Tells More Fortunes Than Ever!

--- main.cpp
+++ main.cpp
@@ -19,6 +19,23 @@ cin>>last;
   lucky = first.length();
 cout<<"Welcome, "<< first[0] << last[0] << " here is your fortune...\n";
 cout<<"Your lucky number is..."<<lucky<<"\n";
+if(first[0]=='A'||first[0]=='a'||first[0]=='E'||first[0]=='e'||first[0]=='I'||first[0]=='i'||first[0]=='O'||first[0]=='o'||first[0]=='U'||first[0]=='u')
+{
+        cout<<"You are destined to be famous!\n";
+}
+else
+{
+        cout<<"You should keep a low profile...\n";
+}
+
+int index = lucky - 1;
+if(last[index]=='A'||last[index]=='a'||last[index]=='E'||last[index]=='e'||last[index]=='I'||last[index]=='i'||last[index]=='O'||last[index]=='o'||last[index]=='U'||last[index]=='u')
+{
+        cout<<"You have already met your true love...\n ";
+}
+
+cout<<"Have a nice day!\n";
+
   return 0;
   
 }
